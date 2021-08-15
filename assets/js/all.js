2d776f675aa65b27addeab2bfacae780c73a1da3
Amy Cheng <amycheng@adbert.com.tw>
2021-08-16 02:48:44 +0800

    Update 2021-08-15T18:48:41.899Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,13 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
